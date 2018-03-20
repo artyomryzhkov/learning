e9226a8ac28857e7991c5412a86b2c9bfe6bdb22
Yusuf Khudayberdiev <yus-uf96@mail.ru>
2018-03-20 16:56:46 +0300

    Added change of views between months and year, active buttons class style  added

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,6 +1,7 @@
 var current_month = new Date().getMonth();
 var current_year = new Date().getFullYear();
 var current_view = 'year';
+var previous_button = 'year_button';
 window.onload = function () {
      var month_year = getCurrentMonth (current_month, current_year);
      var new_days = getCurrentDays (current_month, current_year);
@@ -10,6 +11,8 @@ window.onload = function () {
      if (current_view === 'year')
      {
          createYearView(current_year);
+         document.getElementsByClassName('year_button')[0].classList.add("active_button");
+         previous_button = current_view+'_button';
      }
      document.getElementsByClassName("prev")[0].onclick = function() {
         current_month-=1;
@@ -44,17 +47,31 @@ window.onload = function () {
     document.getElementsByClassName("view_prev")[0].onclick = function() {
         if (current_view === 'year'){
             current_year -= 1;
+            getCurrentViewDate(current_view, current_year,current_month);
+            createYearView(current_year);
         }
-        getCurrentViewDate(current_view, current_year,current_month);
-        createYearView(current_year);
     };
     document.getElementsByClassName("view_next")[0].onclick = function() {
         if (current_view === 'year'){
             current_year += 1;
+            getCurrentViewDate(current_view, current_year,current_month);
+            createYearView(current_year);
         }
-        getCurrentViewDate(current_view, current_year,current_month);
-        createYearView(current_year);
     };
+    document.getElementsByClassName("month_button")[0].onclick = function() {
+        current_view = 'month';
+        document.getElementsByClassName(previous_button)[0].classList.remove('active_button');
+        document.getElementsByClassName('month_button')[0].classList.add('active_button');
+        previous_button = current_view+'_button';
+        document.getElementsByClassName("view")[0].innerHTML = getCurrentDays (current_month, current_year);
+    }
+    document.getElementsByClassName("year_button")[0].onclick = function() {
+        createYearView(current_year);
+        current_view = 'year';
+        document.getElementsByClassName(previous_button)[0].classList.remove('active_button');
+        document.getElementsByClassName('year_button')[0].classList.add('active_button');
+        previous_button = current_view+'_button';
+    }
 
 };
 
